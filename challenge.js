13fe49013142496f6c3d7261e1d64bdbc3c1e2df
RachelIsrael <racheleisrael4@gmail.com>
2020-02-06 10:03:14 -0500

    event listener on rectangles

--- challenge.js
+++ challenge.js
@@ -1,12 +1,18 @@
 let pyramid = document.querySelector('.pyramid');
 for (let i=0; i<45; i++){
     let subject = document.createElement('div');
-    subject.style.height = '20px';
-    subject.style.width = '30px';
+    subject.style.height = '100px';
+    subject.style.width = '120px';
     subject.style.backgroundColor = 'grey';
     pyramid.style.display = 'flex';
     pyramid.style.flexDirection = 'row';
     pyramid.style.flexWrap = 'wrap';
-    pyramid.style.justifyContent = 'space-evenly';
+    pyramid.style.justifyContent = 'space-between';
+    pyramid.style.alignContent = 'center';
+    subject.style.margin = '20px 15px';
+    subject.addEventListener('click', respond)
+    function respond(){
+        console.log('Huzzah!')
+    }
     pyramid.appendChild(subject);
 }
