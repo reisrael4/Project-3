5e40fa8c62d5337b79c8bb7a6225d1d21ccd629d
RachelIsrael <racheleisrael4@gmail.com>
2020-02-06 10:18:05 -0500

    first fetch

--- challenge.js
+++ challenge.js
@@ -1,4 +1,5 @@
 let pyramid = document.querySelector('.pyramid');
+let url = 'http://ron-swanson-quotes.herokuapp.com/v2/quotes'
 for (let i=0; i<45; i++){
     let subject = document.createElement('div');
     subject.style.height = '100px';
@@ -10,9 +11,13 @@ for (let i=0; i<45; i++){
     pyramid.style.justifyContent = 'space-between';
     pyramid.style.alignContent = 'center';
     subject.style.margin = '20px 15px';
+    subject.style.border = '5px solid #7B232E'
     subject.addEventListener('click', respond)
     function respond(){
         console.log('This is an excellent rectangle!')
     }
     pyramid.appendChild(subject);
 }
+fetch(url).then(res=>{
+    console.log('Bully for you!', res)
+})
