673d534d357c24f40e923f3eb6022eb498ad35ab
RachelIsrael <racheleisrael4@gmail.com>
2020-02-06 14:25:20 -0500

    intial modal js

--- challenge.js
+++ challenge.js
@@ -6,25 +6,31 @@ for (let i=0; i<45; i++){
     let subject = document.createElement('div');
     let modal = document.createElement('div');
     let close = document.createElement('span');
+    close.innerHTML = "&times;";
     let modalContent = document.createElement('p');
     modalContent.classList.add('modal-content')
     close.classList.add('close-button');
     modal.classList.add('modal');
     subject.classList.add('pyramid-subject');
     subject.addEventListener('click', respond);
+    close.addEventListener('click', closeModal)
     function respond(e){
         e.preventDefault();
+        modal.style.display = 'block';
         fetch(url)
             .then(res=>{
                 return res.json();
             })
             .then(res=>{
             // console.log('Bully for you!')
-            console.log(res[0]);
+            modalContent.innerText = res[0];
         })
         // console.log('This is an excellent rectangle!')
     }
-    close.innerHTML = "&times;"
+    function closeModal(e){
+        e.preventDefault();
+        modal.style.display = 'none';
+    }
     pyramid.appendChild(subject);
     subject.appendChild(modal);
     modal.appendChild(modalContent);
