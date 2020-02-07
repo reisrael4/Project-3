3ca412e94242e0049e0cf9337ddb2c1d916f5c3b
RachelIsrael <racheleisrael4@gmail.com>
2020-02-07 10:46:10 -0500

    still working on exit animation

--- challenge.js
+++ challenge.js
@@ -28,9 +28,9 @@ for (let i=1; i<=9; i++){
             }
         function closeModal(e){
             e.preventDefault();
-            modal.classList.add('modal-close');
-            //Something with the display is causing the animation to not work. Nest the fucntions!
+            //Something with the display is causing the animation to not work.
             if(e.target == close){
+                modal.classList.add('modal-close');
                 modal.style.display = 'none';
             }
         }
